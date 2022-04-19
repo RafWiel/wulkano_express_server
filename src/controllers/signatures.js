a5bc701f5199bd9225d9f25b36c8ceaad894c449
RafalW <rafal.wielicki@gmail.com>
2022-04-19 09:54:17 +0200

    Save signatures as base64

--- src/controllers/signatures.js
+++ src/controllers/signatures.js
@@ -3,8 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const {Directory} = require('../models');
 
+
 module.exports = {
-  async get (req, res) {
+  async get1 (req, res) {
     try {
       //get directory path
       let directory = await Directory.findOne({
@@ -33,6 +34,33 @@ module.exports = {
           res.set('Content-Type', 'text/plain');
           res.status(404).end('Not found');
       });
+    }
+    catch (error) {
+      tools.sendError(res, error);
+    }
+  },
+  async get (req, res) {
+    try {
+      //get directory path
+      let directory = await Directory.findOne({
+        where : {
+          id: req.query.dir
+         }
+      });
+
+      let filePath = path.join(__dirname, '..', '..', directory.path);
+      filePath = path.join(filePath, req.query.sig);
+
+      //check if exists
+      if (!fs.existsSync(filePath)) {
+        res.set('Content-Type', 'text/plain');
+        res.status(404).end('Not found');
+      }
+
+      //send base64 data
+      const data = fs.readFileSync(filePath, {encoding: 'base64'});
+      res.set('Content-Type', 'text/plain');
+      res.status(200).end(data);
     }
     catch (error) {
       tools.sendError(res, error);
