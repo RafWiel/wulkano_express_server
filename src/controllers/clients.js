9f8d8e77736f3e3d0a6a5b195b82d301cb9bbd62
RafalW <rafal.wielicki@gmail.com>
2022-10-06 10:09:05 +0200

    Add methods to get list of clients names and company names

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,39 +1,89 @@
-const {QueryTypes} = require('sequelize');
-const {sequelize} = require('../models');
 const tools = require('../misc/tools');
 const {Client} = require('../models');
+const { Op } = require('sequelize');
+
+function getFirstByName (req, res) {
+  Client.findOne({
+    where: { name: req.query.name }
+  })
+  .then((item) => sendClient(res, item))
+  .catch((error) => tools.sendError(res, error));
+}
+
+function getFirstByCompanyName (req, res) {
+  Client.findOne({
+    where: { companyName: req.query['company-name'] }
+  })
+  .then((item) => sendClient(res, item))
+  .catch((error) => tools.sendError(res, error));
+}
+
+function getFirstByPhoneNumber (req, res) {
+  Client.findOne({
+    where: { phoneNumber: req.query['phone-number'] }
+  })
+  .then((item) => sendClient(res, item))
+  .catch((error) => tools.sendError(res, error));
+}
+
+function sendClient (res, item) {
+  if (!item) {
+    res.send({ result: false });
+    return;
+  }
+
+  res.send({
+    id: item.id,
+    name: item.name,
+    companyName: item.companyName,
+    phoneNumber: item.phoneNumber,
+  });
+}
 
 module.exports = {
+  getNames (req, res) {
+    Client.findAll({
+      attributes: ['name'],
+      where: {
+        name: {
+          [Op.like]: `%${req.query.filter}%`
+        }
+      }
+    })
+    .then((values) => res.send(values.map(u => u.name)))
+    .catch((error) => tools.sendError(res, error));
+  },
+  getCompanyNames (req, res) {
+    Client.findAll({
+      attributes: ['companyName'],
+      where: {
+        companyName: {
+          [Op.like]: `%${req.query.filter}%`
+        }
+      }
+    })
+    .then((values) => res.send(values.map(u => u.companyName)))
+    .catch((error) => tools.sendError(res, error));
+  },
   getPhoneNumbers (req, res) {
-    sequelize.query(`
-      select
-        phoneNumber
-      from Clients
-      where phoneNumber like :filter
-    `, {
-      type: QueryTypes.SELECT,
-      replacements: { filter: `%${req.query.filter}%` },
+    Client.findAll({
+      attributes: ['phoneNumber'],
+      where: {
+        phoneNumber: {
+          [Op.like]: `%${req.query.filter}%`
+        }
+      }
     })
     .then((values) => res.send(values.map(u => u.phoneNumber)))
     .catch((error) => tools.sendError(res, error));
   },
-  getFirstByPhoneNumber (req, res) {
+  getFirst (req, res) {
     console.log('query', req.query);
-    Client.findOne({
-      where: { phoneNumber: `${req.query['phone-number']}` }
-    })
-    .then((item) => {
-      if (!item) {
-        res.send({ result: false });
-        return;
-      }
+    if (req.query.name) return getFirstByName(req, res);
+    if (req.query['company-name']) return getFirstByCompanyName(req, res);
+    if (req.query['phone-number']) return getFirstByPhoneNumber(req, res);
 
-      res.send({
-        id: item.id,
-        name: item.name,
-        companyName: item.companyName,
-      });
-    })
-    .catch((error) => tools.sendError(res, error));
+    res.send({ result: false });
   },
+
 }
--- src/routes/clients.js
+++ src/routes/clients.js
@@ -2,6 +2,8 @@ const clients = require('../controllers/clients');
 const authorizationMiddleware = require('../middlewares/authorization');
 
 module.exports = (app) => {
+  app.get('/clients/names', authorizationMiddleware.filter, clients.getNames);
+  app.get('/clients/company-names', authorizationMiddleware.filter, clients.getCompanyNames);
   app.get('/clients/phone-numbers', authorizationMiddleware.filter, clients.getPhoneNumbers);
-  app.get('/clients/first', authorizationMiddleware.filter, clients.getFirstByPhoneNumber);
+  app.get('/clients/first', authorizationMiddleware.filter, clients.getFirst);
 }
